7d033040cacf28c6e06a8ff1f1b119db6d25c248
Nicole Zurita <npz2dw@gmail.com>
2017-04-03 15:05:19 -0700

    added practice with boxing and unboxing

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an empty List of type object
+            List<object> basic_list = new List<object>();
+            int int_adder = 0;
+            // Add the following values to the list: 7, 28, -1, true, "chair"
+            basic_list.Add(7);
+            basic_list.Add(28);
+            basic_list.Add(-1);
+            basic_list.Add(true);
+            basic_list.Add("chair");
+            // Loop through the list and print all values (Hint: Type Inference might help here!)
+            for (int i = 0; i < basic_list.Count; i++)
+            {
+                Console.WriteLine(basic_list[i]);
+                if (basic_list[i] is int)
+                {
+                    int_adder += (int)basic_list[i];
+                }
+            }
+
+            Console.WriteLine(int_adder);
+        }
+    }
+}
