625eae769ff0a41d54e4332b0c2527713e7d18ca
Nicole Zurita <npz2dw@gmail.com>
2017-04-06 16:34:23 -0700

    Linq skeleton added. Practice with Linq

---
+++ LinqSkeleton/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            Artist artistFromMtVernon = Artists.Where(artist1 => artist1.Hometown == "Mount Vernon").First();
+            System.Console.WriteLine("Name: " + artistFromMtVernon.ArtistName + " Age: " + artistFromMtVernon.Age);
+
+            //Who is the youngest artist in our collection of artists?
+            Artist youngest_artist = Artists.OrderBy(artist2 => artist2.Age).First();
+            System.Console.WriteLine("Name of youngest artist: " + youngest_artist.ArtistName + " Age: " + youngest_artist.Age);
+
+            //Display all artists with 'William' somewhere in their real name
+            List<Artist> william_artists = Artists.Where(artist3 => artist3.RealName.Contains("William")).ToList();
+            System.Console.WriteLine("Williams in Real Name:");
+            for (int i = 0; i < william_artists.Count; i++)
+            {
+                System.Console.WriteLine(" - " + william_artists[i].RealName + " / " + william_artists[i].ArtistName);
+            }
+
+            // Display all groups with names less than 8 characters in length.
+            List<Group> groups_with_names_lt_8_chars = Groups.Where(group => group.GroupName.Length < 8).ToList();
+            System.Console.WriteLine("Groups with names less than 8 characters in length: ");
+            for (int j = 0; j < groups_with_names_lt_8_chars.Count; j++)
+            {
+                System.Console.WriteLine(" - "+ groups_with_names_lt_8_chars[j].GroupName);
+            }
+
+            //Display the 3 oldest artist from Atlanta
+            List<Artist> all_artists = Artists.Where(artist4 => artist4.Hometown == "Atlanta").OrderBy(artist4 => artist4.ArtistName).Reverse().Take(3).ToList();
+            System.Console.WriteLine("3 Oldest Artists from Atlanta: ");
+            for (int k = 0; k < all_artists.Count; k++)
+            {
+                System.Console.WriteLine(" - " + all_artists[k].ArtistName + " / Age: " + all_artists[k].Age);
+            }
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+        }
+    }
+}
