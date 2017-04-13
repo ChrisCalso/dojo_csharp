567f758b292fc1b3309f6e4c59bb1b6ed2b9d2ea
Nicole Zurita <npz2dw@gmail.com>
2017-04-13 16:04:11 -0700

    form submission assignment using validations

---
+++ form_submission/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using form_submission.Models;
+using System.Linq;
+
+namespace form_submission.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewBag.errors = new List<string>();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("success")]
+        public IActionResult Success(string first_name, string last_name, string age, string email, string password)
+        {
+            User newuser = new User
+            {
+                First_Name = first_name,
+                Last_Name = last_name,
+                Age = age,
+                Email = email,
+                Password = password
+
+            };
+            TryValidateModel(newuser);
+            ViewBag.errors = ModelState.Values;
+            if (ModelState.IsValid)
+            {
+                return View();
+                
+            }
+            
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
